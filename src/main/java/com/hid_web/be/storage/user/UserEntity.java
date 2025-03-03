6d83bdee3a808d41ebf0ad025a691bbe839810ef
givemesodev <givemesodev@gmail.com>
2025-03-03 15:34:57 +0900

    전시 API 테스트 - 3

--- src/main/java/com/hid_web/be/storage/user/UserEntity.java
+++ src/main/java/com/hid_web/be/storage/user/UserEntity.java
@@ -2,7 +2,6 @@ package com.hid_web.be.storage.user;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
